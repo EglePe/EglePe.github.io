a902bdf9f17c64e73a6ba658b8f9c00208c8f00c
EglePe <34908596+EglePe@users.noreply.github.com>
2018-01-10 09:51:31 +0200

    Update task2.php

--- task2.php
+++ task2.php
@@ -5,7 +5,6 @@ function plytele(){
  $a = 7;
  $plytelesPlotis = $i * $a;
   return $plytelesPlotis;
-  echo "Plyteles plotis: ".$plytelesPlotis."<br />";
 }
 
 //BASEINO FUNKCIJA
@@ -16,7 +15,6 @@ $a = 15;
  
 $baseinoPlotis = $i * $p + ($i * $a) * 2 + ($p * $a) *2;
   return $baseinoPlotis;
-  echo "Baseino bendras plotis yra: ".$baseinoPlotis."<br />";
   
 }
 
@@ -24,8 +22,9 @@ $baseinas = baseinas($baseinoPlotis);
 $plytele = plytele($plytelesPlotis);
 
 $plyteliuKiekis = $baseinas / $plytele;
-echo "Plyteles plotis: ".$baseinas."<br />";
-echo "Baseino bendras plotis yra: ".$plytele."<br />";
+echo "Plyteles plotis: ".$plytele."<br />";
+echo "Baseino bendras plotis yra: ".$baseinas."<br />";
 echo "Is viso reikes ".ceil($plyteliuKiekis)." plyteliu.";
+
 
 ?>
